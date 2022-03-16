548dcfeb45a9b74aadb0054b4bed8f48d3e296bb
Matthieu Clay <matthieu.clay@outlook.fr>
2022-03-16 10:17:43 +0100

    :sparkles: improve post component

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { addPosts } from "../actions/post.action";
 
 const PostForm = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const user = useSelector((state) => state.userReducer);
+  const dispatch = useDispatch();
+
+  const handleForm = (e) => {
+    e.preventDefault();
+
+    if (title && content) {
+      const data = {
+        title,
+        content,
+        author: user[0].pseudo,
+        likes: 0,
+      };
+
+      dispatch(addPosts(data));
+      setTitle("");
+      setContent("");
+    }
+  };
+
   return (
     <div className="form-container">
-      <form>
-        <input type="text" placeholder="Titre du poste" />
-        <textarea placeholder="Postez vos pensées..."></textarea>
+      <form onSubmit={(e) => handleForm(e)}>
+        <input
+          type="text"
+          placeholder="Titre du poste"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <textarea
+          placeholder="Postez vos pensées..."
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        ></textarea>
         <input type="submit" value="Envoyer" />
       </form>
     </div>
--- src/reducers/post.reducer.js
+++ src/reducers/post.reducer.js
@@ -1,4 +1,4 @@
-import { GET_POSTS } from "../actions/post.action";
+import { GET_POSTS, ADD_POSTS } from "../actions/post.action";
 
 const initialState = {};
 
@@ -6,6 +6,8 @@ export default function postReducer(state = initialState, action) {
   switch (action.type) {
     case GET_POSTS:
       return action.payload;
+    case ADD_POSTS:
+      return [action.payload, ...state];
     default:
       return state;
   }
